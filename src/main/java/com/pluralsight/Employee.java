d4becd495fe1577b164224f2bd730ab071343d47
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 03:29:55 -0500

    add properties, constructors, get/set

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class Employee {
+
+    //added properties
+    float id;
+    String name;
+    float hoursWorked;
+    float payRate;
+
+    //add constructors
+
+    public Employee(float id, String name, float hoursWorked, float payRate) {
+        this.id = id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    //add get/set
+
+
+    public float getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public float getPayRate() {
+        return payRate;
+    }
 }
